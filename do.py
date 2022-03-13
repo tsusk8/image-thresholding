17f666591ea8ebf65ddf11dc10a2ba358db3cc4c
Tsubasa <tsubasa@fukuitsubasanoMacBook-Pro.local>
2022-03-13 15:00:08 +0900

    Unify variable names

--- do.py
+++ do.py
@@ -3,86 +3,61 @@ import glob
 from tkinter import filedialog
 import pathlib
 
-# 閾値の設定
-threshold = 4
-
-# ファイル拡張子の設定
-fTyp = [("all files", "*"), ("jpeg", "*.jpeg"), ("JPEG", "*.JPEG")]
+threshold  = 4
+file_types = [("all files", "*"), ("jpeg", "*.jpeg"), ("JPEG", "*.JPEG")]
 
 # ファイルを一枚選択する
 def selectAFile():
     global threshold
-    global fTyp
+    global file_types
 
-    # ファイル取得
-    file = filedialog.askopenfilename(filetypes=fTyp)
+    file = filedialog.askopenfilename(file_types=file_types)
+    img  = cv2.imread(file)
 
-    # jpegファイルの読み込み
-    img = cv2.imread(file)
-
-    # しきい値処理の実施
     controllThresholding(file, img)
 
 def selectFiles():
     global threshold
-    global fTyp
+    global file_types
 
-    # ファイル取得
-    files = filedialog.askopenfilenames(filetypes=fTyp)
+    files = filedialog.askopenfilenames(file_types=file_types)
     
-    # ループ文で処理を実施
     loopThresholding(files)
 
 # ループ文で1枚ずつ処理を実施
 def loopThresholding(files):
     for file in files:
-        # jpegファイルの読み込み
         img = cv2.imread(file)
 
-        # しきい値処理の実施
         controllThresholding(file, img)
 
 # フォルダーを選択する
 def selectADirectory():
     global threshold
 
-    # 対象のフォルダー選択
     target_path = filedialog.askdirectory()
+    files       = glob.glob(target_path+'/*.jpeg')
 
-    # 対象のフォルダー内のjpegのみを取得
-    files = glob.glob(target_path+'/*.jpeg')
-
-    # ループ文で処理を実施
     loopThresholding(files)
         
-# しきい値処理の実施
+# 閾値処理の実施
 def doThresholding(img):
-    # cvtColorで変換かける
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # バイナリー化
-    ret, binary = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
+    gray   = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    binary = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
 
     return binary
 
 def controllThresholding(file, img):
-    # jpegファイル変換処理
-    binary = doThresholding(img)
-    
-    # ファイル名取得
+    binary    = doThresholding(img)
     file_name = pathlib.PurePath(file).stem
 
-    # 画像保存処理
     cv2.imwrite('thresholding-' + file_name + '.jpeg', binary)
 
-print('how many select your picture?')
+print('Enter "1" if only one photo is to be processed.')
+print('Enter "2" for multiple photos.')
+print('To select all the photos in a folder, enter any other value.')
+
 choose = input()
-
-# 使い方
-
-# 写真を1枚だけでチェックしたい場合は、「1」を入力
-# 写真を複数枚チェックしたい場合は、「2」を入力
-# 写真をフォルダーごとチェックしたい場合は「1」、「2」以外を入力
 
 if (choose == '1'):
     selectAFile()
